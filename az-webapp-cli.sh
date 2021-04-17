fde13e80eb23a2517b99ad9f21d8f46bdf170562
Wilson Mar <64391825+wilson-mar@users.noreply.github.com>
2021-04-16 20:33:00 -0600

    Update az-webapp-cli.sh

--- az-webapp-cli.sh
+++ az-webapp-cli.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-# This script sample is part of "Learn Azure in a Month of Lunches - 2nd edition" (Manning
-# Publications) by Iain Foulds.
-#
-# This sample script covers the exercises from chapter 3 of the book. For more
-# information and context to these commands, read a sample of the book and
-# purchase at https://www.manning.com/books/learn-azure-in-a-month-of-lunches-second-edition
-#
-# This script sample is released under the MIT license. For more information,
-# see https://github.com/fouldsy/azure-mol-samples-2nd-ed/blob/master/LICENSE
+# az-webapp-cli.sh
+# This script was adapted from https://github.com/fouldsy/azure-mol-samples-2nd-ed/blob/master/15/azure_cli_sample.sh
+# released under the MIT license. See https://github.com/fouldsy/azure-mol-samples-2nd-ed/blob/master/LICENSE
+# and chapter 15 of the ebook "Learn Azure in a Month of Lunches - 2nd edition" (Manning Publications) by Iain Foulds,
+# Purchase at https://www.manning.com/books/learn-azure-in-a-month-of-lunches-second-edition
 
 # Define variables for unique Web App name.
 # As we create DNS for the Web App, the DNS name must be unique. By adding some
@@ -18,26 +14,26 @@
 webAppName=azuremol$RANDOM
 
 # Create a resource group
-az group create --name azuremolchapter3 --location eastus
+az group create --name "${MY_RG}" --location "${MY_LOC}"
 
 # Create an App Service plan
 # An App Service plan defines the location and available features
 # These features include deployment slots, traffic routing options, and
 # security options
 az appservice plan create \
-    --resource-group azuremolchapter3 \
     --name appservice \
-    --sku S1
-
+    --sku S1 \
+    --resource-group "${MY_RG}"
+    
 # Create a Web App in the App Service plan enabled for local Git deployments
 # The Web App is what actually runs your web site, lets you create deployment
 # slots, stream logs, etc.
 az webapp create \
-    --resource-group azuremolchapter3 \
     --name $webAppName \
     --plan appservice \
-    --deployment-local-git
-
+    --deployment-local-git \
+    --resource-group "${MY_RG}"
+    
 # Create a Git user accout and set credentials
 # Deployment users are used to authenticate with the App Service when you
 # upload your web application to Azure
@@ -54,15 +50,16 @@ git init && git add . && git commit -m "Pizza"
 
 # Add your Web App as a remote destination in Git
 git remote add azure $(az webapp deployment source config-local-git \
-    --resource-group azuremolchapter3 \
-    --name $webAppName -o tsv)
-
+    --name $webAppName -o tsv) \
+    --resource-group "${MY_RG}"
+    
 # Push, or upload, the sample app to your Web App
 git push azure master
 
 # Get the hostname of the Web App
 # This hostname is set to the variable hostName and output to the screen in the next command
-hostName=$(az webapp show --resource-group azuremolchapter3 --name $webAppName --query defaultHostName --output tsv)
-
+hostName=$(az webapp show --name $webAppName --query defaultHostName --output tsv) \
+    --resource-group "${MY_RG}"
+    
 # Now you can access the Web App in your web browser
 echo "To see your Web App in action, enter the following address in to your web browser:" $hostName
