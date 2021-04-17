9c2d4a52b5aa8689989a814f0e9cc91d386d8210
Wilson Mar <64391825+wilson-mar@users.noreply.github.com>
2021-04-17 06:40:36 -0600

    Update az-vm-cli.sh

--- az-vm-cli.sh
+++ az-vm-cli.sh
@@ -1,30 +1,28 @@
 #!/usr/bin/env bash
 
-# This script is adapted from "Learn Azure in a Month of Lunches - 2nd edition" (Manning
-# Publications) by Iain Foulds.
-#
-# This sample script covers the exercises from chapter 2 of the book. For more
-# information and context to these commands, read a sample of the book and
-# purchase at https://www.manning.com/books/learn-azure-in-a-month-of-lunches-second-edition
-#
-# This script sample is released under the MIT license. For more information,
-# see https://github.com/fouldsy/azure-mol-samples-2nd-ed/blob/master/LICENSE
-
-# Generate SSH keys
-# SSH keys are used to securely authenticate with a Linux VM
-# This is somewhat optional, as the Azure CLI can generate keys for you
-ssh-keygen -t rsa -b 2048
-
-# View the public part of your SSH key
-# From the CLI, you don't really need this. But if you use the Azure portal or
-# Resource Manager templates (which we look at in chapter 6), you need to
-# provide this public key
-cat .ssh/id_rsa.pub
+# az-vm-cli.sh
+# This script was adapted from https://github.com/fouldsy/azure-mol-samples-2nd-ed/blob/master/02/azure_cli_sample.sh
+# released under the MIT license. See https://github.com/fouldsy/azure-mol-samples-2nd-ed/blob/master/LICENSE
+# and chapter 2 of the ebook "Learn Azure in a Month of Lunches - 2nd edition" (Manning Publications) by Iain Foulds,
+# Purchase at https://www.manning.com/books/learn-azure-in-a-month-of-lunches-second-edition
 
 # Create a resource group. This is a logical container to hold your resources.
 # You can specify any name you wish, so long as it's unique with your Azure
 # subscription and location
 az group create --name "${MY_RG}" --location "${MY_LOC}"
+
+# Generate SSH key pair using built-in Linux ssh-keygen program in folder
+# /home/wilson/.ssh/"${SSH_KEY_FILE_NAME}"  # (instead of file id_rsa)
+# SSH keys are used to securely authenticate with a Linux VM
+# This is somewhat optional, as the Azure CLI can generate keys for you
+ssh-keygen -t rsa -b 2048 -f "${SSH_KEY_FILE_NAME}" -N ""
+   # -N ""  makes the command not prompt for manual input.
+
+# View the public part of your SSH key pair:
+# From the CLI, you don't really need this. But if you use the Azure portal or
+# Resource Manager templates (which we look at in chapter 6), you need to
+# provide this public key
+cat .ssh/"${SSH_KEY_FILE_NAME}"
 
 # Create a Linux VM
 # You specify the resoure group from the previous step, then provide a name.
@@ -32,16 +30,16 @@ az group create --name "${MY_RG}" --location "${MY_LOC}"
 # The `--generate-ssh-keys` checks for keys you may have created earlier. If
 # SSH keys are found, they are used. Otherwise, they are created for you
 az vm create \
-    --name webvm \
+    --name "${MY_VM_NAME}" \
     --image UbuntuLTS \
-    --admin-username azuremol \
+    --admin-username "${MY_ADMIN_USER_NAME}" \
     --generate-ssh-keys \
     --resource-group "${MY_RG}"
 
 # Obtain the public IP address of your VM. Enter the name of your resource
 # group and VM if you changed them
 publicIp=$(az vm show \
-    --name webvm \
+    --name "${MY_VM_NAME}" \
     --show-details \
     --query publicIps \
     --output tsv) \
@@ -50,14 +48,15 @@ publicIp=$(az vm show \
 # TODO: if publicIp is blank, stop
 
 # SSH to your VM with the username and public IP address for your VM
-ssh azuremol@$publicIp
+ssh "${MY_ADMIN_USER_NAME}"@$publicIp
 
 # Once logged in to your VM, install the LAMP web stack with apt-get
 sudo apt update && sudo apt install -y lamp-server^
 logout
 
-# Open port 80 to your webserver
-az vm open-port --name webvm --port 80 --resource-group "${MY_RG}"
+# Open port 80 to your webserver (not HTTPS) while testing:
+az vm open-port --name "${MY_VM_NAME}" --port 80 --resource-group "${MY_RG}"
+# TODO: Enable TLS for port 443
 
 # Now you can access the basic website in your web browser
 echo "To see your web server in action, enter the public IP address in to your web browser: http://$publicIp"
