9468e7e1efc541d5a48c6f3a949fe24031725d9d
Wilson Mar <64391825+wilson-mar@users.noreply.github.com>
2021-04-17 21:55:11 -0600

    Update az-aks-cli.sh

--- az-aks-cli.sh
+++ az-aks-cli.sh
@@ -11,14 +11,14 @@ set -o errexit
 # Create a resource group
 az group create --name "${MY_RG}" --location "${MY_LOC}"
 
-# Create a Dockerfile:
+echo "<<< Create a Dockerfile:"
 cat <<EOF > Dockerfile
 FROM nginx:1.17.5
 EXPOSE 80:80
 COPY index.html /usr/share/nginx/html
 EOF
 
-# Create an Azure Container Instance
+echo "<<< Create an Azure Container Instance:"
 # A public image from Dockerhub is used as the source image for the container,
 # and a public IP address is assigned. To allow web traffic to reach the 
 # container instance, port 80 is also opened
@@ -29,14 +29,14 @@ az container create \
     --ports 80 \
     --resource-group "${MY_RG}"
 
-# Show the container instance public IP address
+echo "<<< Show the container instance public IP address:"
 az container show \
     --name "${MY_CONTAINER}" \
     --query ipAddress.ip \
     --output tsv \
     --resource-group "${MY_RG}"
 
-# Create an Azure Container Service with Kubernetes (AKS) cluster
+echo "<<< Create an Azure Container Service with Kubernetes (AKS) cluster:"
 # Two nodes are created. 
 az aks create \
   --name "${MY_CONTAINER}" \
@@ -46,7 +46,7 @@ az aks create \
     --resource-group "${MY_RG}"
 # It can take ~10 minutes for this operation to successfully complete.
 
-# Get the AKS credentials
+echo "<<< Get the AKS credentials:"
 # This gets the Kuebernetes connection information and applies to a local
 # config file. You can then use native Kubernetes tools to connect to the
 # cluster.
@@ -57,7 +57,7 @@ az aks get-credentials \
 # (Idempotent) Install the kubectl CLI for managing the Kubernetes cluster
 az aks install-cli
 
-# Start an Kubernetes deployment
+echo "<<< Start an Kubernetes deployment:"
 # This deployment uses the same base container image as the ACI instance in
 # a previous example. Again, port 80 is opened to allow web traffic.
 kubectl run "${MY_CONTAINER}" \
@@ -66,7 +66,7 @@ kubectl run "${MY_CONTAINER}" \
     --port=80 \
     --generator=run-pod/v1
 
-# Create a load balancer for Kubernetes deployment
+echo "<<< Create a load balancer for Kubernetes deployment:"
 # Although port 80 is open to the deployment, external traffic can't reach the
 # Kubernetes pods that run the containers. A load balancer needs to be created
 # that maps external traffic on port 80 to the pods. Although this is a
@@ -76,19 +76,19 @@ kubectl expose deployment/"${MY_CONTAINER}" \
     --type="LoadBalancer" \
     --port 80
     
-# View the public IP address of the load balancer
+echo "<<< View the public IP address of the load balancer:"
 # It can take 2-3 minutes for the load balancer to be created and the public
 # IP address associated to correctly direct traffic to the pod
 kubectl get service
 
-# Scale out the number of nodes in the AKS cluster
+echo "<<< Scale out the number of nodes in the AKS cluster:"
 # The cluster is scaled up to 3 nodes
 az aks scale \
     --name "${MY_CONTAINER}" \
     --node-count 3 \
     --resource-group "${MY_RG}"
 
-# Scale up the number of replicas
+echo "<<< Scale up the number of replicas:"
 # When our web app container was deployed, only one instance was created. Scale
 # up to 5 instances, distributed across all three nodes in the cluster
 kubectl scale deployment "${MY_CONTAINER}" --replicas 5
