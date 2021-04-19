d7ab6bf2ec6b00d54c3df99797338fd9524d25c3
Wilson Mar <64391825+wilson-mar@users.noreply.github.com>
2021-04-19 08:41:00 -0600

    Create az-functions-temp.sh

--- az-functions-temp.sh
+++ az-functions-temp.sh
@@ -7,8 +7,7 @@
 # Purchase at https://www.manning.com/books/learn-azure-in-a-month-of-lunches-second-edition
 #
 # There are quite a few components here that aren't available in the Azure CLI
-# The complete examples from chapter 21 fill in the gaps with the use of the
-# Azure portal.
+# The complete examples from chapter 21 fill in the gaps with use of the Azure portal GUI.
 # As such, this is not a complete example script that provides a functional end
 # result at the end of the script.
 
@@ -19,6 +18,7 @@ az group create --name "${MY_RG}" --location "${MY_LOC}"
 
 # Define a unique name for the Service Bus namespace
 MY_SVC_BUS_NAMESPACE=$MY_SVC_BUS_NAME$RANDOM
+echo "MY_SVC_BUS_NAMESPACE=$MY_SVC_BUS_NAMESPACE"
 
 # Create a Service Bus namespace
 # This namespace is used to then create a queue that allows messages to be
@@ -31,8 +31,8 @@ az servicebus namespace create --name "${MY_SVC_BUS_NAMESPACE}" \
 # This queue is used to connect Azure IoT Hub with your serverless applications
 # to pass messages back and forth
 az servicebus queue create \
-    --namespace-name "${MY_SVC_BUS_NAMESPACE}" \
     --name "${MY_SVC_BUS_NAME}" \
+    --namespace-name "${MY_SVC_BUS_NAMESPACE}" \
     --resource-group "${MY_RG}"
     
 # Define a unique name for the Storage account
