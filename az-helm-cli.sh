4078762ebd0b6706f52b9da779e11260cf0a951e
Wilson Mar <64391825+wilson-mar@users.noreply.github.com>
2021-04-19 19:43:18 -0600

    Update az-helm-cli.sh

--- az-helm-cli.sh
+++ az-helm-cli.sh
@@ -7,7 +7,7 @@
 # ???
 # Based on # https://docs.microsoft.com/en-us/azure/container-registry/container-registry-oci-artifacts
 #    0. Define environment variables before invoking this script
-#       MY_LOC, MY_RG, MY_ACR, --username $SP_APP_ID --password $SP_PASSWD
+#       MY_LOC, MY_RG, MY_ACR, --username $SP_APP_ID --password $SP_PASSWD, MY_ACR_REPO
 #    1. Navigate/create container folder and download this repo into it
 #    2. Install and start the Docker client if it's not already installed and started
 #    3. Install and use CLI to log into Azure
@@ -23,15 +23,19 @@
 
 #   12. Install https://github.com/deislabs/oras to use the OCI Registry as Storage (ORAS) tool to
 #   13. Use ORAS to push the new image into your ACR (Azure Container Registry), instead of DockerHub.
-#   14. Get attributes of an artifact in ACR, to confirm.
+#   14. Remove the image tag from your local Docker environment.
+#   15. List repos (artifacts) in ACR, to confirm:
+#   16. List tags in ACR, to confirm:"
+#   17. Get attributes of an artifact in ACR, to confirm.
 
-#   15. Reference Helm3 charts as OCI artifacts in the ACR (Azure Container Registry). 
+#   18a. Run individual Docker image or "
+#   18b. Reference Helm3 charts as OCI artifacts in the ACR (Azure Container Registry). 
 #          The OCI (Open Container Initiative) Image Format Specs is at https://github.com/opencontainers/distribution-spec
-#   16. Use ACR tasks to build and test container images.
-#   17. Install Helm, get helm version.
-#   18. Push change into Helm to trigger run which establishes services in Azure.
-#   19. Validate automation.
-#   20. Have Azure Defender Security Center scan images in ACR:"
+#   19. Use ACR tasks to build and test container images.
+#   20. Install Helm, get helm version.
+#   21. Push change into Helm to trigger run which establishes services in Azure.
+#   22. Validate automation.
+#   23. Have Azure Defender Security Center scan images in ACR:"
 #
 # Authenticate with your registry using the helm registry login command.
 # Use helm chart commands in the Helm CLI to push, pull, and manage Helm charts in a registry
@@ -58,12 +62,26 @@ az login  # pops-up browser
 
 echo ">>> 04. Create your private ACR (Azure Container Registry):"
 # https://docs.microsoft.com/en-us/azure/container-registry/container-registry-get-started-azure-cli
-az acr create --sku Basic \
-  --name "${MY_ACR}" \
-  --resource-group "${MY_RG}"
+# https://docs.microsoft.com/en-us/azure/container-registry/container-registry-tutorial-prepare-registry
 
-  # TODO: In output, capture to $GEND_ACR_LOGIN_SERVER
+# See https://docs.microsoft.com/en-us/cli/azure/acr?view=azure-cli-latest
+RESPONSE=$( az acr check-name -n "${MY_ACR}" )
+# if RESPONSE = exists:
+   az acr create --sku Basic \
+      --name "${MY_ACR}" \
+      --resource-group "${MY_RG}"
+# For more parms, see https://docs.microsoft.com/en-us/cli/azure/acr?view=azure-cli-latest
+
+
+# TODO: In output, capture to $GEND_ACR_LOGIN_SERVER
   # "loginServer": "mycontainerregistry007.azurecr.io",
+#fi
+
+# Service Tier: see https://docs.microsoft.com/en-us/azure/container-registry/container-registry-skus#changing-tiers
+# az acr update --name "${MY_ACR}" --sku Standard
+         # or --sku Premium   
+         # or --sku Standard  # for increased storage and image throughput
+         # or --sku Basic     # for best cost savings
 
 echo ">>> 05. Login into your ACR:"
 az acr login --name "${MY_ACR}"
@@ -99,19 +117,46 @@ oras push "${MY_ACR}".azurecr.io/samples/artifact:1.0 \
    # Pushed myregistry.azurecr.io/samples/artifact:1.0
    # Digest: sha256:xxxxxxbc912ef63e69136f05f1078dbf8d00960a79ee73c210eb2a5f65xxxxxx
 
-echo ">>> 14. Get attributes of an artifact in ACR, to confirm:"
+echo ">>> 14. Remove the image tag from your local Docker environment."
+# (Note that this docker rmi command does not remove the image from the hello-world repository in your Azure container registry.)
+docker rmi <login-server>/hello-world:v1
+
+echo ">>> 15. List repos (artifacts) in ACR, to confirm:"
+az acr repository list --name "${MY_ACR}" --output table
+
+echo ">>> 16. List tags in ACR, to confirm:"
+az acr repository show-tags --name "${MY_ACR}" --repository "${MY_ACR_REPO}" --output table
+
+export registry="jasonacrr.azurecr.io"
+export user="jasonacrr"
+export password="t4AH+K86xxxxxxx2SMxxxxxzjNAMVOFb3c" 
+export operation="/v2/aci-helloworld/tags/list" 
+export credentials=$(echo -n "$user:$password" | base64 -w 0) 
+export catalog=$(curl -s -H "Authorization: Basic $credentials" https://$registry$operation)
+echo "Catalog"
+echo $catalog
+
+
+echo ">>> 17. Get attributes of an artifact in ACR, to confirm:"
 az acr repository show \
     --name "${MY_ACR}" \
     --image samples/artifact:1.0.  # ???
 
-echo ">>> 15. Reference Helm3 charts as OCI artifacts in the ACR (Azure Container Registry):"
-#          The OCI (Open Container Initiative) Image Format Specs is at https://github.com/opencontainers/distribution-spec"
-echo ">>> 16. Use ACR tasks to build and test container images."
-echo ">>> 17. Install Helm, get helm version."
-echo ">>> 18. Push change into Helm to trigger run which establishes services in Azure."
-echo ">>> 19. Validate automation."
-echo ">>> 20. Have Azure Defender Security Center scan images in ACR:"
+echo ">>> 18a. Run individual Docker image or "
+docker run <login-server>/hello-world:v1
 
-echo ">>> xx. Have Azure Defender Security Center scan images in ACR:"
+echo ">>> 18b. Reference Helm3 charts as OCI artifacts in the ACR (Azure Container Registry):"
+#          The OCI (Open Container Initiative) Image Format Specs is at https://github.com/opencontainers/distribution-spec"
+
+echo ">>> 19. Use ACR tasks to build and test container images."
+echo ">>> 20. Install Helm, get helm version."
+echo ">>> 21. Push change into Helm to trigger run which establishes services in Azure."
+echo ">>> 22. Validate automation."
+
+echo ">>> 23. Have Azure Defender Security Center scan images in ACR:"
 # https://docs.microsoft.com/en-us/azure/security-center/defender-for-container-registries-introduction?bc=/azure/container-registry/breadcrumb/toc.json&toc=/azure/container-registry/toc.json
+
+echo ">>> 24. Clean up resource group, ACR, images, then list Resource Groups:"
+az group delete --name "${MY_RG}"
+az group list
 
